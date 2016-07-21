9d9b4be29b1ee1e7a0540395b0e447760b2e210f
Ignacio <i.burgos@coolblue.nl>
2016-07-21 16:43:17 +0200

    make NgFor loop work

--- app/components/projectList.ts
+++ app/components/projectList.ts
@@ -2,28 +2,28 @@ import {bootstrap} from 'angular2/platform/browser';
 import {Component} from 'angular2/core';
 import {NgFor} from 'angular2/common';
 import {Project} from '../entities/project';
-import {ProjectRepository} from '../repositories/projectRepository';
+// import {ProjectRepository} from '../repositories/projectRepository';
 
 @Component({
     selector: 'project-list',
     // templateUrl: './templates/projectlist.html',
     template:`
     <ul>
-        <li *ngFor="let project of projects">
+        <li *ngFor="#project of projects">
             <p>{{project.id}}</p>
             <p>{{project.name}}</p>
         </li>
     </ul>`,
-    directives: [],
-    providers: [ProjectRepository]
+    directives: [NgFor]
+    // providers: [ProjectRepository]
 })
 
 export class ProjectList {
     projects: Project[] = [{ id:1, name:"project one"}, { id:2, name:"project two"}];
 
-    constructor(private projectRepository: ProjectRepository) {
+    // constructor(private projectRepository: ProjectRepository) {
 
-    }
+    // }
 
     ngOnInit() {
         //this.projectRepository.getProjects().subscribe(projects => this.projects = projects[0]);
