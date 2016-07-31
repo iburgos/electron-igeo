f0667bedb32042d981a0649c8f7ae264a0e93150
i-burgos <i.burgos@coolblue.nl>
2016-07-31 23:01:16 +0200

    Load Fake project json data using repository

--- app/components/projectList.ts
+++ app/components/projectList.ts
@@ -1,30 +1,25 @@
 import {Component} from '@angular/core';
 import {Project} from '../entities/project';
-// import {ProjectRepository} from '../repositories/projectRepository';
+import {ProjectRepository} from '../repositories/projectRepository';
+import { HTTP_PROVIDERS } from '@angular/http';
 
 @Component({
     selector: 'project-list',
-    // templateUrl: './templates/projectlist.html',
-    template:`
-    <ul>
-        <li *ngFor="let project of projects">
-            <p>{{project.id}}</p>
-            <p>{{project.name}}</p>
-        </li>
-    </ul>`,
-    directives: []
-    // providers: [ProjectRepository]
+    templateUrl: '../templates/projectlist.html',
+    directives: [],
+    providers: [ProjectRepository, HTTP_PROVIDERS]
 })
 
 export class ProjectList {
-    projects: Project[] = [{ id:1, name:"project one"}, { id:2, name:"project two"}];
+    
+    projects: Project[];
 
-    // constructor(private projectRepository: ProjectRepository) {
+    constructor(private projectRepository: ProjectRepository) {
 
-    // }
+    }
 
     ngOnInit() {
-        //this.projectRepository.getProjects().subscribe(projects => this.projects = projects[0]);
-
+        this.projectRepository.getProjects()
+            .subscribe(data => this.projects = data);
     }
 }
