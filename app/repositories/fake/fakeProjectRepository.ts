e7daa46ae3e5df26b23a79020efec51e91962f7c
i-burgos <i.burgos@coolblue.nl>
2016-08-01 00:03:39 +0200

    fix fake repository

--- app/repositories/fake/fakeProjectRepository.ts
+++ app/repositories/fake/fakeProjectRepository.ts
@@ -1,6 +1,6 @@
 import {Injectable} from '@angular/core';
 import {Http} from '@angular/http';
-import {Project} from './app/entities/project';
+import {Project} from '../../entities/project';
 
 @Injectable()
 export class ProjectRepository {
@@ -8,7 +8,7 @@ export class ProjectRepository {
     constructor(private http: Http) { }
 
     getProjects() {
-        return this.http.get('app/repositories/fake/fakeprojectlist.json')
-            .map(response => <Project[]>response.json());
+        return this.http.get('./repositories/fake/fakeProjectList.json')
+            .map(response => <Project[]>response.json().data);
     }
 }
