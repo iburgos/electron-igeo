c3ea15f916f987d9a8968dd55b661d1aef7f942c
i-burgos <i.burgos@coolblue.nl>
2016-08-01 22:58:35 +0200

    Add sqlite database file

---
+++ app/data-access/repositories/projectRepository.ts
@@ -0,0 +1,15 @@
+import {Injectable} from '@angular/core';
+import {Http} from '@angular/http';
+import {Project} from '../../entities/project';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class ProjectRepository {
+
+    constructor(private http: Http) { }
+
+    getProjects() {
+        return this.http.get('./repositories/fake/fakeProjectList.json')
+            .map(response => <Project[]>response.json().data);
+    }
+}
