a5e1e3395a19ba93c6c657727ed14418e1e69427
iburgos <ignacio.burgos.escribano@gmail.com>
2016-08-21 14:48:00 +0200

    Remove unused code from projectRepository

--- app/repositories/projectRepository.ts
+++ app/repositories/projectRepository.ts
@@ -12,10 +12,6 @@ export class ProjectRepository {
         let headers = new Headers({ 'Content-Type': 'application/json' });
         let options = new RequestOptions({ headers: headers });
         let url = 'http://localhost:53677/igeoservice/project/all';
-        console.log("here");
-        let httpResponse = this.http.get(url, options).map(response => console.log(response));
-        console.log(httpResponse.lift);
-        httpResponse.forEach(project => console.log(project));
         return this.http.get(url, options)
             .map(response => <Project[]>response.json());
     }
